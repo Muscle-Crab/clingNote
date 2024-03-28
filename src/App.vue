db7d7fd06f067bc9de9b0180d26f4a848c97d1ea
daltonsmith <ds7513635@gmail.com>
2024-03-28 03:31:08 -0400

    Change name

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About Changes</router-link>
+    <router-link to="/about">About Crab</router-link>
   </nav>
   <router-view/>
 </template>
