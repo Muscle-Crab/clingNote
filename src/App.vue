7d9f3608819a73784e475dfe064e93672e4798c3
daltonsmith <ds7513635@gmail.com>
2024-03-28 04:12:53 -0400

    Change name

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">Dalton</router-link>
+    <router-link to="/about">One Signal</router-link>
   </nav>
   <router-view/>
 </template>
