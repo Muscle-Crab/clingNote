23be834b9ac5bd14f0cacb0f4a6b657a90305399
daltonsmith <ds7513635@gmail.com>
2024-03-28 03:18:27 -0400

    first chage

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,23 @@
   </nav>
   <router-view/>
 </template>
+<script setup>
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  const OneSignal = window.OneSignal || [];
+  OneSignal.push(() => {
+    OneSignal.init({
+      appId: "c9aceaa3-3b9b-453c-a3e3-e28dcea47b48",
+      safari_web_id: "web.onesignal.auto.599df49f-4478-46e8-9089-3c95d63b63cf",
+      notifyButton: {
+        enable: true,
+      },
+      subdomainName: "1gtfwsaafsds",
+    });
+  });
+});
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
