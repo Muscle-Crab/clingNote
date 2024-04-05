2fad436352fd992798af3c540c6081c2203cb423
daltonsmith <ds7513635@gmail.com>
2024-04-05 18:55:47 -0400

    Change name

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <button @click="speak">Click to Hear Voice Response</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    speak() {
+      const message = new SpeechSynthesisUtterance('Hello, this is an auto voice response.');
+      window.speechSynthesis.speak(message);
+    }
+  }
+};
+</script>
