70b4702fce3a3e9efb0a7d7cd74f9683a6886372
daltonsmith <ds7513635@gmail.com>
2024-04-06 14:06:14 -0400

    Change name

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 
-createApp(App).use(store).use(router).mount('#app')
+// Initialize the Vue app
+const app = createApp(App);
+
+// Use plugins
+app.use(store);
+app.use(router);
+app.use(OneSignalVuePlugin, {
+    appId: '65d866ad-f59c-4557-9d75-4ccf7fe60a47',
+});
+
+// Mount the app
+app.mount('#app');
