59e953c3721cb7d3294de5431a2018fdc06d5f34
daltonsmith <ds7513635@gmail.com>
2024-05-13 13:49:16 -0400

    update navi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-
+    <navi/>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
+import navi from '@/components/nav.vue'
 </script>
 
 <style scoped>
+
 /* Your CSS styles here */
 </style>
