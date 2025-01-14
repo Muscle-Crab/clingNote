5807f6e779bff8ed5cbe862c98d416c65d1d0b9e
daltonsmith <ds7513635@gmail.com>
2025-01-14 12:23:30 -0500

    for unauthenticated users

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -178,14 +178,13 @@
 </template>
 
 <script setup>
-import {ref, onMounted, computed, reactive} from 'vue';
+import { ref, onMounted, computed, reactive } from 'vue';
 import axios from 'axios'
 import navi from '@/components/nav.vue'
 import data from '@/data.json';
 import draggable from "vuedraggable";
 import {db} from '@/firebaseConfig'; // Assuming you have imported the Firebase setup file and exported the db instance
 import {collection, doc, setDoc, serverTimestamp, getDoc, updateDoc} from 'firebase/firestore';
-
 const modalOpen = ref(false);
 
 
@@ -207,6 +206,9 @@ const formatTimes = (index) => {
   const seconds = currentTime.value[index] % 60;
   return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 };
+
+
+
 
 
 const openModal = () => {
@@ -282,6 +284,7 @@ const getSelectedDayRoutine = () => {
 };
 
 
+
 const updateRoutine = () => {
   selectedDayRoutine.value = getSelectedDayRoutine();
   fetchSelectedDayRoutine();
@@ -339,15 +342,17 @@ const toggleTaskCompletion = async (index) => {
       console.log('Task completion updated successfully');
 
       // Voice response when task is completed
-      setTimeout(() => {
-        if (task.completed) {
-          const completionMessage = `Great job! You completed the task: ${task.title}.`;
-          speak(completionMessage);
-        } else {
-          const uncompletionMessage = `You have marked the task: ${task.title} as incomplete.`;
-          speak(uncompletionMessage);
-        }
-      }, 100); // Short delay to ensure user interaction context
+
+    }
+    if (task.completed) {
+      const nextTaskIndex = selectedDayRoutine.value.findIndex(t => !t.completed && t.title !== task.title);
+
+      if (nextTaskIndex !== -1) {
+        const nextTask = selectedDayRoutine.value[nextTaskIndex];
+        const completionMessage = `Task ${task.title} completed. It's time to begin the next task, ${nextTask.title}.`;
+        speak(completionMessage);
+        showNotification.value = true;
+      }
     }
   } catch (error) {
     console.error('Error updating task completion:', error);
@@ -547,9 +552,9 @@ const showNotification = ref(false);
 
 // List of predefined badges
 const badges = [
-  {days: 1, name: 'Consistency Starter', description: 'Completed tasks for 3 days in a row!', icon: '🔥'},
-  {days: 7, name: 'Streak Warrior', description: 'Completed tasks for 7 days in a row!', icon: '🏅'},
-  {days: 30, name: 'Master of Routine', description: 'Completed tasks for 30 days in a row!', icon: '💪'}
+  { days: 1, name: 'Consistency Starter', description: 'Completed tasks for 3 days in a row!', icon: '🔥' },
+  { days: 7, name: 'Streak Warrior', description: 'Completed tasks for 7 days in a row!', icon: '🏅' },
+  { days: 30, name: 'Master of Routine', description: 'Completed tasks for 30 days in a row!', icon: '💪' }
 ];
 
 
@@ -665,6 +670,7 @@ const checkForBadges = () => {
 onMounted(() => {
   checkStreakOnCompletion();
 });
+
 
 
 </script>
