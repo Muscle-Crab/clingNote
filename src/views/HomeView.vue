10fc1a8698395642305e34547c246776f73b325e
daltonsmith <ds7513635@gmail.com>
2025-01-20 09:28:46 -0500

    animated progress bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -767,14 +767,14 @@ const toggleTaskCompletion = async (index) => {
   if (task.completed) {
     userCredits.value += 10; // Award 10 credits for completing a task
     console.log(`Credits earned: 10. Total credits: ${userCredits.value}`);
-    speak(`Great job! You completed the task: ${task.title}.`);
+    // speak(`Great job! You completed the task: ${task.title}.`);
     announceNextTask(index); // Pass the index of the completed task
     const userName = await fetchUserName(userId.value);
     if (userName) {
       await sendNotificationToPlayer(userName, "completed");
     }
   } else {
-    speak(`You have marked the task: ${task.title} as incomplete.`);
+    // speak(`You have marked the task: ${task.title} as incomplete.`);
     userCredits.value -= 10; // Deduct credits if task is marked incomplete
     console.log(`Credits deducted: 10. Total credits: ${userCredits.value}`);
   }
