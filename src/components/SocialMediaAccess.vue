8e17151da75be3964be7fd9dad9f511e2e8b217a
daltonsmith <ds7513635@gmail.com>
2025-03-13 04:10:29 -0400

    change position of won't do tasks

--- src/components/SocialMediaAccess.vue
+++ src/components/SocialMediaAccess.vue
@@ -37,46 +37,22 @@ const props = defineProps({
 
 // Define social media platforms with app deep links
 const socialPlatforms = [
-  {
-    id: "tiktok",
-    name: "TikTok",
-    icon: "https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg",
-    app: "tiktok://",
-    web: "https://www.tiktok.com",
-  },
-  {
-    id: "instagram",
-    name: "Instagram",
-    icon: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png",
-    app: "instagram://",
-    web: "https://www.instagram.com",
-  },
-  {
-    id: "youtube",
-    name: "YouTube",
-    icon: "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg",
-    app: "vnd.youtube://",
-    web: "https://www.youtube.com",
-  },
-  {
-    id: "facebook",
-    name: "Facebook",
-    icon: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
-    app: "fb://",
-    web: "https://www.facebook.com",
-  },
+  { id: "tiktok", name: "TikTok", icon: "https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg", app: "tiktok://", web: "https://www.tiktok.com" },
+  { id: "instagram", name: "Instagram", icon: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png", app: "instagram://", web: "https://www.instagram.com" },
+  { id: "youtube", name: "YouTube", icon: "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg", app: "vnd.youtube://", web: "https://www.youtube.com" },
+  { id: "facebook", name: "Facebook", icon: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg", app: "fb://", web: "https://www.facebook.com" },
 ];
 
 // Function to open social media apps on mobile
 const attemptToOpenSocialMedia = (platform) => {
   if (props.completionPercentage < 100) return; // Prevent access if tasks are incomplete
 
-  // Try opening the app first
+  // Try opening the app using window.location.href (direct user action, no pop-ups)
   window.location.href = platform.app;
 
-  // If the app fails to open, fallback to the web version
+  // If the app fails, redirect to the web version after a delay
   setTimeout(() => {
-    window.open(platform.web, "_blank");
-  }, 1200);
+    window.location.href = platform.web;
+  }, 1500);
 };
 </script>
