f340566355d2173267855c7ca8741adb1905741e
daltonsmith <ds7513635@gmail.com>
2025-04-06 14:22:49 -0400

    change position of won't do tasks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1167,7 +1167,7 @@ const sendNotificationToPlayer = async (userName, action) => {
 
   const data = {
     "app_id": "fc206a71-7d65-4cfa-b8b2-0c10548e1476", // Replace with your actual OneSignal app ID
-    "include_player_ids": ["ff823cf5-aef7-4363-82f7-33c1de7ce02e"], // Replace with the user's device/player ID
+    "include_player_ids": ["3581ab6a-69f2-4328-9090-bc36e71c3869"], // Replace with the user's device/player ID
     "contents": { "en": actionMessages[action]?.content || "An action was performed." },
     "headings": { "en": actionMessages[action]?.heading || "Notification" }
   };
