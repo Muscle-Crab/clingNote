12f9a1d03345e64c741dfaedbc1574924ed2c99a
daltonsmith <ds7513635@gmail.com>
2025-05-23 11:19:19 -0400

    change position of won't do tasks

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -17,7 +17,7 @@ exports.handler = async (event) => {
                         product_data: {
                             name: 'AI Routine Access',
                         },
-                        unit_amount: 5.79, // $5.00
+                        unit_amount: 500, // $5.00
                     },
                     quantity: 1,
                 },
