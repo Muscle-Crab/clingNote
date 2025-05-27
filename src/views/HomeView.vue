3560d39a9e752b9b63f4639c2eb09c5c98f730d8
daltonsmith <ds7513635@gmail.com>
2025-05-27 10:37:48 -0400

    change position of won't do tasks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -730,7 +730,7 @@
                 <draggable
                     handle=".drag-handle"
                     :animation="150"
-                    :list="sortedSelectedDayRoutine"
+                    v-model="selectedDayRoutine"
                     tag="div"
                     class="tasks-list space-y-2"
                     ghost-class="ghost"
